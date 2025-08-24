124358b8d4dd3466747f330c2bae3cc443d29d04
Dominic P E <dominicpe2k04@gmail.com>
2025-08-24 18:53:43 +0530

    Refactor network.py to simplify Wi-Fi connection logic
    Removed socket handling and related logic.

--- network.py
+++ network.py
@@ -8,14 +8,6 @@ wifi.active(True)
 ssid = 'Common'
 password = 'password'
 
-def open_socket():
-    adress = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
-    s = socket.socket()
-    s.bind(adress)
-    s.listen(3)
-    return(s)
-
-
 while True:
     # Wifi Connection code
     if wifi.isconnected():
@@ -28,14 +20,4 @@ while True:
             print("Wi-Fi is connected:", wifi.ifconfig()[0])
     # Wifi code over
     
-    if wifi.isconnected():
-        try:
-            s = open_socket()
-            client = s.accept()[0]
-            request = client.recv(1024)
-            request = str(request)
-            print(request)
-        except OSError as e:
-            print("error")
-#         
     time.sleep(1)
